ac03d1c13146409b8425fac5dc43cdc0f269ff01
Tvoje Jmenod <josefhruska2@gmail.com>
2026-03-16 14:16:51 +0100

    porovnana radkovani a treti makro

--- bitarray.h
+++ bitarray.h
@@ -12,15 +12,24 @@
 typedef unsigned long bitarray_t[];
 typedef unsigned long bitarray_index_t;
 
-#define bitarray_create(jmeno_pole, velikost) unsigned long \
- jmeno_pole[((velikost) + (sizeof(unsigned long) * CHAR_BIT) - 1)/ (sizeof(unsigned long) * CHAR_BIT) + 1] = {(velikost), 0};\
- static_assert((velikost) > 0, "Velikost pole musi byt kladna")
+//pomocne makrko na vzorec
+#define BITARRAY_BLOCKS(velikost) (((velikost) + (sizeof(unsigned long) * CHAR_BIT) - 1) / (sizeof(unsigned long) * CHAR_BIT) + 1)
+
+#define bitarray_create(jmeno_pole, velikost) \
+    unsigned long jmeno_pole[BITARRAY_BLOCKS(velikost)] = {(velikost), 0};\
+    static_assert((velikost) > 0, "Velikost pole musi byt kladna")
 //celociselne deleni musi zaokrouhlit nahoru, ne dolu a pridame jednu bunku pro 0 z toho vychazi vzorec
 
-#define bitarray_alloc(jmeno_pole, velikost) unsigned long \
-*jmeno_pole = calloc((((velikost) + (sizeof(unsigned long) * CHAR_BIT) - 1) / (sizeof(unsigned long) * CHAR_BIT) + 1),\
-sizeof(unsigned long)); assert((velikost) > 0); if(jmeno_pole == NULL){fprintf(stderr, "bitarray_alloc: Chyba alokace pameti\n");\
-exit(1); jmeno_pole[0] = (velikost)}
+#define bitarray_alloc(jmeno_pole, velikost) \
+    unsigned long *jmeno_pole = calloc(BITARRAY_BLOCKS(velikost),sizeof(unsigned long));\
+    assert((velikost) > 0 && (velikost) <= (ULONG_MAX - (sizeof(unsigned long) * CHAR_BIT)));\
+    if(jmeno_pole == NULL) { \
+        fprintf(stderr, "bitarray_alloc: Chyba alokace pameti\n"); \
+        exit(1);\
+    }\
+    jmeno_pole[0] = (velikost)
+
+#define bitarray_free(jmeno_pole)
 
 
 #endif
