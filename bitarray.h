a3b2b4e574497eec9d6afff7284552a1112b88ed
Tvoje Jmenod <josefhruska2@gmail.com>
2026-03-16 10:03:53 +0100

    inicialni bitarray.h

---
+++ bitarray.h
@@ -0,0 +1,18 @@
+#ifndef BITARRAY_H
+#define BITARRAY_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdlib.h>
+
+typedef unsigned long bitarray_t[];
+
+
+typedef unsigned long bitarray_index_t;
+
+
+
+
+#endif
