d843b461dd2a915953db2c874766df52e69c5c64
Tvoje Jmenod <josefhruska2@gmail.com>
2026-03-18 13:12:40 +0100

    makra i errory done

--- error.c
+++ error.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include <std>
+#include <stdlib.h>
+#include "error.h"
 
-int main() {
-    
-    return 0;
+void warning(const char *fmt, ...){
+    fprintf(stderr, "Warning: ");
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+    fprintf(stderr, "\n");
 }
+
+void error_exit(const char *fmt, ...){
+    fprintf(stderr, "Error: ");
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 
