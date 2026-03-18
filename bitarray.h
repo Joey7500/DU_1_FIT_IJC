be442dd1765a42ccdf8fce74543053698ba67b36
Tvoje Jmenod <josefhruska2@gmail.com>
2026-03-18 12:10:50 +0100

    bitarray.h hotovy

--- bitarray.h
+++ bitarray.h
@@ -19,7 +19,7 @@ typedef unsigned long bitarray_index_t;
 //create
 #define bitarray_create(jmeno_pole, velikost) \
     unsigned long jmeno_pole[BITARRAY_BLOCKS(velikost)] = {(velikost), 0};\
-    static_assert((velikost) > 0, "Velikost pole musi byt kladna")
+static_assert((velikost) > 0, "Velikost pole musi byt kladna")
 //celociselne deleni musi zaokrouhlit nahoru, ne dolu a pridame jednu bunku pro 0 z toho vychazi vzorec
 
 //alloc
@@ -51,10 +51,38 @@ inline void bitarray_fill(bitarray_t jmeno_pole, int bool_vyraz) {
         }
     }
 }
+inline void bitarray_setbit(bitarray_t jmeno_pole, bitarray_index_t index, int bool_vyraz) {
+#ifndef NO_CHECK
+    if (index >= bitarray_size(jmeno_pole)) {
+        error_exit("bitarray_setbit: Index %lu mimo rozsah 0..%lu", 
+                   (unsigned long)index, 
+                   (unsigned long)(bitarray_size(jmeno_pole) - 1));
+    }
+#endif
 
-inline void
+    if (bool_vyraz) {
+        jmeno_pole[(index / (sizeof(unsigned long) * CHAR_BIT)) + 1] |= 
+        (1UL << (index % (sizeof(unsigned long) * CHAR_BIT)));
+    } else {
+        jmeno_pole[(index / (sizeof(unsigned long) * CHAR_BIT)) + 1] &= 
+        ~(1UL << (index % (sizeof(unsigned long) * CHAR_BIT)));
+    }
+}
 
-#else
+inline int bitarray_getbit(bitarray_t jmeno_pole, bitarray_index_t index) {
+#ifndef NO_CHECK
+    if (index >= bitarray_size(jmeno_pole)) {
+        error_exit("bitarray_getbit: Index %lu mimo rozsah 0..%lu", 
+                   (unsigned long)index, 
+                   (unsigned long)(bitarray_size(jmeno_pole) - 1));
+    }
+#endif
+
+    return (jmeno_pole[(index / (sizeof(unsigned long) * CHAR_BIT)) + 1] & 
+           (1UL << (index % (sizeof(unsigned long) * CHAR_BIT)))) != 0;
+}
+
+#else //neinline fce
 
 //size
 #define bitarray_size(jmeno_pole) jmeno_pole[0]
@@ -104,17 +132,17 @@ inline void
 
 //musime pouzit operator carka
     #define bitarray_getbit(jmeno_pole, index) \
-        (index >= bitarray_size(jmeno_pole)) ? \
+        ((index >= bitarray_size(jmeno_pole)) ? \
         (error_exit("bitarray_getbit: Index %lu mimo rozsah 0..%lu",(unsigned long)(index),\
-        (unsigned long) (bitarray_size(jmeno_pole)-1)),0): 0 \
+        (unsigned long) (bitarray_size(jmeno_pole)-1)),0): 0, \
         (jmeno_pole[((index)/(sizeof(unsigned long) * CHAR_BIT)) + 1] & \
-        (1UL << ((index) % (sizeof(unsigned long) * CHAR_BIT)))) != 0
+        (1UL << ((index) % (sizeof(unsigned long) * CHAR_BIT)))) != 0)
 
 #else
 //pokud je definovany no check
     #define bitarray_getbit(jmeno_pole, index) \
-        (jmeno_pole[((index)/(sizeof(unsigned long) * CHAR_BIT)) + 1] & \
-        (1UL << ((index) % (sizeof(unsigned long) * CHAR_BIT)))) != 0
+        ((jmeno_pole[((index)/(sizeof(unsigned long) * CHAR_BIT)) + 1] & \
+        (1UL << ((index) % (sizeof(unsigned long) * CHAR_BIT)))) != 0)
 
 #endif //konec NO_CHECK
 
